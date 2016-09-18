533ab565808ee019c8bf7d1b344b90d37b6691cd
Danika Harada <danikaharada@danikas-air.gateway.ht.net>
2016-09-17 17:16:30 -1000

    exercise 4

--- js/app.js
+++ js/app.js
@@ -50,7 +50,15 @@ element.className is a property that sets or returns the class name of an elemen
 
 var getBuckets = ["Become more famous than that overrated fraud Garfield", "Visit Aoshima (aka Japan's Cat Island)", "Exhaust my entire 9 lives", "Pass DevLeague's coding challenge", "Become the next internet sensation", "eat tacos", "drinks gin", "read books", "sleep", "Drink coffee", "eat chips"];
 
+for (var i = 0; i < getBuckets.length; i++){
+	console.log(getBuckets[i]);
 
+var bucketList = document.createElement('li');
+bucketList.className = "daList";
+bucketList.innerHTML = getBuckets[i];
+buckets.appendChild(bucketList);
+
+}
 
 //Exerise 4
 /*Our motivated Tacocat has been hard at work on its bucket list.  Update the current list to the following:
@@ -60,7 +68,9 @@ var getBuckets = ["Become more famous than that overrated fraud Garfield", "Visi
 Declare a variable named changeList and assign it to the getElementsByTagName method.
 */
 
+var changeList = document.getElementsByTagName('li');
 
+changeList [0].innerHTML = "I'm coming for you Felix!";
 
 
 
